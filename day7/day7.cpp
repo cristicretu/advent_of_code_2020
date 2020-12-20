40a5f3d9e38262e068e85e90c33a1161ce0a539b
cristicretu <cristicretu38@gmail.com>
2020-12-20 21:55:10 +0200

    test

---
+++ day7/day7.cpp
@@ -0,0 +1,23 @@
+/**
+ *    author: etohirse
+ *    created: 20.12.2020 20:54:52
+ **/
+#include <algorithm>
+#include <fstream>
+#include <map>
+#include <string>
+#include <vector>
+
+using rule = std::map<std::string, int>;
+
+std::ifstream fin("input.in");
+std::ofstream fout("input.out");
+
+int main() {
+  std::map<std::string, rule> rules;
+  std::string line;
+  while (geline(line)) {
+    line.erase
+  }
+  return 0;
+}
